d77a2ed08bb1656dfb5204a1bbf8944aec8802e1
Yo@hotmail.com <you@example.com>
2017-03-01 11:22:26 +0100

    project : add seeds and columns in model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Project.destroy_all
+
+
+projects = Project.create!( [ {name: "Filtre à eau", description: "Fournir de l'eau potable à l'école", location: "Asharympathy", pictures_goal: 100, project_picture: "", current: true, done: false, future: false, impact: 30},
+                      {name: "Forage", description: "Fournir de l'eau potable à un village", location: "Theni", pictures_goal: 1000, project_picture: "", current: false, done: false, future: true, impact: 700},
+                      {name: "Filtre à eau", description: "Fournir de l'eau potable à l'école", location: "Asharympathy", pictures_goal: 100, project_picture: "", current: false, done: true, future: false, impact: 30}
+                      ] )
