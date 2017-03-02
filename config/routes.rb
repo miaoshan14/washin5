239f85cb312802ead13423426ac9730b9d38d0bc
Horsell <ihorsell@protonmail.ch>
2017-03-02 16:24:58 +0000

    aucune modif

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "projects#index"
 
-
   mount Attachinary::Engine => "/attachinary"
 end
