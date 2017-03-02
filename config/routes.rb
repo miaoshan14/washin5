7f86866aac3af070a302f5380a9fda18cc4effa8
Horsell <ihorsell@protonmail.ch>
2017-03-02 17:46:41 +0000

    Add Oauthfacebook

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     resources :participations, only: [:create, :new]
   end
 
-  devise_for :users
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "projects#index"
 
