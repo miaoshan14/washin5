049601f6350b3d4b40984066add089f504a87116
Horsell <ihorsell@protonmail.ch>
2017-03-03 10:41:56 +0000

    integration sign up on top login

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users,
-             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "projects#index"
 
