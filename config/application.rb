926ad858d82b65a2a48bd2ef98d53c8e48b29700
Vincent Gass <vintsou@hotmail.com>
2017-03-06 17:49:10 +0100

    modification form signup et passeword reset

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Washin5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
   end
 end
