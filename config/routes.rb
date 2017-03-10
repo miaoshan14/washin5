285a85073fe8af1c00ce55e5c4a79933eb724c61
miaoshan14 <clairedv@hotmail.com>
2017-03-10 09:44:37 +0100

    test https

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "projects#index"
 
+  get "https" => "pages#https"
 
   mount Attachinary::Engine => "/attachinary"
 
